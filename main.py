7624bb7d95f9af328239e6708d258dbcbd168acd
viktorbo <borisoof13@yandex.ru>
2019-12-04 14:15:00 +0300

    [RESTART] Project was refactored

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import os
 import logging as log
-from color_transformation import rgb2gray
-from image_readers import read_img_as_arr
 from datetime import datetime
+from pathlib import Path
 from PIL import Image
+import cv2 as cv
+import argparse
 
 log.basicConfig(level=log.DEBUG,
-
                 format=('{} [%(levelname)s] %(message)s').format(str(datetime.now().strftime('%d-%m-%Y %H:%M:%S'))))
 
 def main():
     logger = log.getLogger(__name__)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, help='Path to input image')
+    args = parser.parse_args()
 
-    img = read_img_as_arr('/home/viktorbo/localdata/datasets/flower_photos/tulips/489506904_9b68ba211c.jpg')
-    img_gray = rgb2gray(img, 'mid')
+    if args.input is None:
+        default_file_name = 'tulips.jpg'
+        default_path = os.path.abspath(default_file_name)
+        logger.warning('The default input file will be used.')
+        input_path = default_path
+    else:
+        input_path = os.path.abspath(args.input)
+
+    assert os.path.exists(input_path), logger.error('Input file {} does not exist!'.format(input_path))
+    logger.info('Input file: {}'.format(input_path))
+
 
 
 
